fe884ee7c22392f1239c1fba8087403a8b55b9a6
Alena <alenademy7@gmail.com>
2022-12-08 13:22:30 +0500

    Добавила решение задачи 27

---
+++ task27/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber(int numberN)
+  {
+
+    int N = Convert.ToString(numberN).Length;
+    int SumNumber = 0;
+    int result = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+      SumNumber = numberN - numberN % 10;
+      result = result + (numberN - SumNumber);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+  int sumNumber = SumNumber(numberN);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+
+
